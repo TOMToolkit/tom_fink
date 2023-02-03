e1b2f340a078a0f47e163883ba4f66638fcf642b
Lindy Lindstrom <llindstrom@lco.global>
2023-02-03 23:41:20 +0000

    allow for compatible point releases of tomtoolkit

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     use_scm_version=False,
     setup_requires=['setuptools_scm', 'wheel'],
     install_requires=[
-        'tomtoolkit>=2.7,~2',
+        'tomtoolkit>=2.7,>=2.12',
         'elasticsearch-dsl>=7.3,<7.5',
         'markdown',
     ],

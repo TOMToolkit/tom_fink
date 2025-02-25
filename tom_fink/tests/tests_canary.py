2862061081e7f67eb1efe0e7185863da584277ba
Tim Lister <tlister@lco.global>
2025-02-25 13:39:34 -0800

    Fix flake8 whinges

--- tom_fink/tests/tests_canary.py
+++ tom_fink/tests/tests_canary.py
@@ -13,6 +13,7 @@ class TestFinkModuleCanary(TestCase):
     def test_boilerplate(self):
         self.assertTrue(True)
 
+
 @tag('canary')
 class TestFinkModuleSSOCanary(TestCase):
     """Tests for the Solar System Object (SSO) search functionality for Issue #98
@@ -21,16 +22,17 @@ class TestFinkModuleSSOCanary(TestCase):
 
     def setUp(self):
         self.broker = FinkBroker()
-        self.parameters = { "query_name": "29P",
-                            "broker": "Fink",
-                            "objectId": "",
-                            "conesearch": "",
-                            "classsearch": "",
-                            "classsearchdate": "",
-                            "ssosearch": "29P"}
+        self.parameters = {"query_name": "29P",
+                           "broker": "Fink",
+                           "objectId": "",
+                           "conesearch": "",
+                           "classsearch": "",
+                           "classsearchdate": "",
+                           "ssosearch": "29P"}
 
     def test_correctkeys(self):
-        expected_keys = ['i:ssnamenr', 'i:candid', 'i:dec', 'i:jd', 'i:magpsf', 'i:objectId', 'i:ra', 'd:roid', 'sso_name', 'sso_number']
+        expected_keys = ['i:ssnamenr', 'i:candid', 'i:dec', 'i:jd', 'i:magpsf',
+                         'i:objectId', 'i:ra', 'd:roid', 'sso_name', 'sso_number']
         data = self.broker.fetch_alerts(self.parameters)
         with self.assertRaises(StopIteration):
             while True:
